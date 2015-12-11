9f1ec58f80977b4ca684cca2f35ffb82b658f36e
Bill Schofield <bill@billschofield.net>
2015-12-11 16:33:25 -0600

    Grammar fixes

--- groovy/src/main/groovy/com/thoughtworks/jcprogram/functional/exercise/StringReducer.groovy
+++ groovy/src/main/groovy/com/thoughtworks/jcprogram/functional/exercise/StringReducer.groovy
@@ -1,7 +1,7 @@
 package com.thoughtworks.jcprogram.functional.exercise
 
 // Given a list of strings like {"Bill", "Archer", "Lana"}
-// Print the first letter of each of the string that have a length of 4.
+// Print the first letter of each of the strings that has a length of 4.
 // For example, "BL"
 
 Set strings = ["Bill", "Archer", "Lana"]
